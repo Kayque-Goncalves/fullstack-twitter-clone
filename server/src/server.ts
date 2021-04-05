198a3e51831f6cf8c8672d90a6188212e4bdd365
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-04-05 11:39:55 -0300

    Adds server initial setup

---
+++ server/src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PROT || 5000
+
+app.listen(PORT, () => console.log(`Server running on port: ${ PORT }`))
+
+app.get('/', (req, res) => {
+    res.send('Everything is working fine.')
+})
