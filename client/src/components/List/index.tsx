05cbb7790695ee5f1f759b15fce8d44be4b30add
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-04-06 14:52:10 -0300

    Creates List component

---
+++ client/src/components/List/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { Container, Item, Title } from './styles'
+
+interface Props {
+  title: string;
+  elements: React.ReactNode[];
+}
+
+const List: React.FC<Props> = ({ title, elements }: Props) => {
+  return (
+    <Container>
+      <Item>
+        <Title>{ title }</Title>
+      </Item>
+      {elements.map((element) => (
+        <Item key="">{ element }</Item>
+      ))}
+    </Container>
+  )
+}
+
+export default List
