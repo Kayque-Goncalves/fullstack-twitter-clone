182210c4a41ec7fa64c825d3ee1293398446a012
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-04-07 15:36:25 -0300

    Adds pages routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import { Layout } from './components/Layout'
+import Auth from './components/Auth'
 import GlobalStyles from './styles/GlobalStyles'
 
 const App: React.FC = () => {
   return (
     <>
-      <Layout />
-
-      <GlobalStyles />
+      <BrowserRouter>
+        <GlobalStyles />
+          <Switch>
+            <Route path="/home" exact component={ Layout } />
+            <Route path="/" exact component={ Auth } />
+          </Switch>
+      </BrowserRouter>
     </>
   )
 }
