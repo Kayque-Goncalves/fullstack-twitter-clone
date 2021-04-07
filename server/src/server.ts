50f7fbd6e56bbdb668faf2f03b365a8a95d01782
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-04-07 15:30:45 -0300

    Setup: routes, mongodb connection and body-parser

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,31 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+
+import userRoutes from './routes/user'
+// import postRoutes from './routes/post'
 
 const app = express()
 const PORT = process.env.PROT || 5000
+dotenv.config()
 
-app.listen(PORT, () => console.log(`Server running on port: ${ PORT }`))
+app.use(bodyParser.json({ limit: "30mb" }))
+app.use(bodyParser.urlencoded({ limit: "30mb" }))
+app.use(cors())
+
+app.get('/user', userRoutes)
+// app.get('/posts', postRoutes)
 
 app.get('/', (req, res) => {
     res.send('Everything is working fine.')
 })
+
+
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${ PORT }`)))
+    .catch((error) => console.log(error))
+
+mongoose.set('useFindAndModify', false)
