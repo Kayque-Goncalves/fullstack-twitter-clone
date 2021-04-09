0b666442211d954cbfc0482ac08fd67ad0e82fb2
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-04-09 15:15:28 -0300

    Refactor: SignupModal behavior

--- client/src/components/SignupModal/index.tsx
+++ client/src/components/SignupModal/index.tsx
@@ -1,103 +1,125 @@
+/* eslint-disable multiline-ternary */
 import React, { useContext, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 
 import { AuthContext } from '../../Contexts/AuthContext'
-import { Container, ModalContent, Header, ArrowLeft, NextButton, Logo, Body, Input, Footer } from './styles'
+import { Container, ModalContent, Header, ArrowLeft, NextButton, Logo, Body, Input, PasswordInput, SubmitButton, Footer } from './styles'
+import { signup } from '../../actions/auth'
+
+let type = 'password'
 
 const SignupModal: React.FC = () => {
   const { closeModal } = useContext(AuthContext)
 
+  const dispatch = useDispatch()
+  const history = useHistory()
+
   const [signStep, setSignStep] = useState(1)
+  const [userSignOption, setUserSignOption] = useState(0)
+  const [formData, setFormData] = useState({ name: '', loginOption: '', month: '', day: '', year: '', password: '', confirmPassword: '' })
+  const [showPassword, setShowPassword] = useState(false)
+
+  function handleChange (e: { target: { name: string; value: string } }) {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  function handleSubmit (e: { preventDefault: () => void }) {
+    e.preventDefault()
+
+    dispatch(signup(formData, history))
+  }
 
   function nextStep () {
     setSignStep(2)
   }
 
-  if (signStep === 1) {
-    return (
+  function prevStep () {
+    setSignStep(1)
+  }
+
+  function changeUserSignOption () {
+    if (userSignOption === 0) setUserSignOption(1)
+    if (userSignOption === 1) setUserSignOption(0)
+  }
+
+  function handleShowPassword () {
+    if (!showPassword) setShowPassword(true)
+    if (showPassword) setShowPassword(false)
+
+    !showPassword ? type = 'password' : type = 'text'
+  }
+
+  return (
       <Container>
 
         <ModalContent>
 
           <Header>
-            <ArrowLeft onClick={ closeModal }/>
+            { signStep === 1 ? <ArrowLeft onClick={ closeModal }/> : <ArrowLeft onClick={ prevStep }/> }
             <Logo />
-            <NextButton onClick={ nextStep }>Next</NextButton>
+            { signStep === 1 && (formData.name.length > 0 && formData.month.length > 0 && formData.day.length > 0 && formData.year.length > 0) && <NextButton onClick={ nextStep }>Next</NextButton> }
           </Header>
 
           <Body>
             <form>
-              <h1>Create your account</h1>
-              <Input placeholder="Name" />
-              <Input placeholder="Phone" />
-              <span>Use email instead</span>
+              { signStep === 1 ? (
+                <>
+                  <h1>Create your account</h1>
+                  <Input name="name" placeholder="Name" required onChange={ handleChange } value={ formData?.name } />
+                  { userSignOption === 0 ? <Input name="loginOption" placeholder="Phone" onChange={ handleChange } value={ formData?.loginOption } /> : <Input name="loginOption" placeholder="Email" required onChange={ handleChange } value={ formData?.loginOption } /> }
+                  <span onClick={ changeUserSignOption }>Use { userSignOption === 0 ? 'Email' : 'Phone' } instead</span>
 
-              <h2>Date of birth</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde aliquid possimus quisquam
-                voluptatibus consequuntur.
-              </p>
+                  <h2>Date of birth</h2>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde aliquid possimus quisquam
+                    voluptatibus consequuntur.
+                  </p>
+
+                  <Footer>
+                    <div>
+                      <span>Month</span>
+                      <select name="month" required onChange={ handleChange } value={ formData?.month }>
+                        <option></option>
+                        <option>October</option>
+                      </select>
+                    </div>
+
+                    <div>
+                      <span>Day</span>
+                      <select name="day" required onChange={ handleChange } value={ formData?.day }>
+                        <option></option>
+                        <option>31</option>
+                      </select>
+                    </div>
+
+                    <div>
+                      <span>Year</span>
+                      <select name="year" required onChange={ handleChange } value={ formData?.year }>
+                        <option></option>
+                        <option>2002</option>
+                      </select>
+                    </div>
+                  </Footer>
+                </>
+              ) : (
+                <>
+                  <h1>You will need a password</h1>
+                  <PasswordInput type={ type } name="password" placeholder="Password" required onChange={ handleChange } value={ formData?.password } />
+                  <PasswordInput type={ type } name="confirmPassword" placeholder="Confirm Password" required onChange={ handleChange } />
+                  <span onClick={ handleShowPassword }>{ type === 'password' ? 'Reveal' : 'Hide' } password</span>
+
+                  { formData.password.length > 0 && <SubmitButton onClick={handleSubmit}>Create my account</SubmitButton>}
+                </>
+              )}
+
             </form>
           </Body>
-
-          <Footer>
-            <div>
-              <span>Month</span>
-              <select>
-              <option></option>
-                <option>October</option>
-              </select>
-            </div>
-
-            <div>
-              <span>Day</span>
-              <select>
-                <option></option>
-                <option>31</option>
-              </select>
-            </div>
-
-            <div>
-              <span>Year</span>
-              <select>
-              <option></option>
-                <option>2002</option>
-              </select>
-            </div>
-          </Footer>
 
         </ModalContent>
 
       </Container>
-    )
-  } else if (signStep === 2) {
-    return (
-        <Container>
-
-        <ModalContent>
-
-          <Header>
-            <Logo />
-          </Header>
-
-          <Body>
-            <form>
-              <h1>You will need a password</h1>
-              <p>Make sure it is 8 characters or more</p>
-              <Input placeholder="Password" />
-              <Input placeholder="Confirm password" />
-
-              {/* <FinishButton>Create Account</FinishButton> */}
-            </form>
-          </Body>
-
-          <Footer>
-          </Footer>
-
-        </ModalContent>
-
-      </Container>
-    )
-  }
+  )
 }
 
 export default SignupModal
