1b3f1679bc4cd82d7e5c44dfc699bd38465b7f93
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-04-09 15:20:19 -0300

    Bug fix: remove body-parser and app.get

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
-import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 
 import userRoutes from './routes/user'
@@ -11,18 +10,16 @@ const app = express()
 const PORT = process.env.PROT || 5000
 dotenv.config()
 
-app.use(bodyParser.json({ limit: "30mb" }))
-app.use(bodyParser.urlencoded({ limit: "30mb" }))
+app.use(express.json({ limit: "30mb" }))
+app.use(express.urlencoded({ limit: "30mb" }))
 app.use(cors())
 
-app.get('/user', userRoutes)
+app.use('/user', userRoutes)
 // app.get('/posts', postRoutes)
 
-app.get('/', (req, res) => {
-    res.send('Everything is working fine.')
-})
-
-
+// app.get('/', (req, res) => {
+//     res.send('Everything is working fine.')
+// })
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${ PORT }`)))
