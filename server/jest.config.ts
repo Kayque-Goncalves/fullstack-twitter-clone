72392d9ef975ee5fef25a909c24bcfde28192cdf
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-04-14 10:10:59 -0300

    Deletes old jest.config file

--- server/jest.config.ts
+++
@@ -1,9 +0,0 @@
-import { compilerOptions } from './tsconfig.json'
-import { pathsToModuleNameMapper } from 'ts-jest/utils'
-
-export default {
-  clearMocks: true,
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' }),
-  preset: 'ts-jest',
-  testEnvironment: 'node'
-}
