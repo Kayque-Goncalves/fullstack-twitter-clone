1f791958addc811048d9307607e48ade130be832
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-04-14 10:03:50 -0300

    Adds react-router-dom path to login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,10 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import { Layout } from './components/Globals/Layout'
 import Auth from './components/Auth'
+import Login from './components/Auth/Login'
+
 import GlobalStyles from './styles/GlobalStyles'
+
 import { AuthProvider } from './Contexts/AuthContext'
 
 const App: React.FC = () => {
@@ -11,13 +14,13 @@ const App: React.FC = () => {
     <>
       <BrowserRouter>
         <GlobalStyles />
-          <Switch>
-              <Route path="/home" exact component={ Layout } />
-            <AuthProvider>
-              <Route path="/" exact component={ Auth } />
-            {/* <Route path="/login" exact component={ Login } /> */}
-            </AuthProvider>
-          </Switch>
+        <Switch>
+          <Route path="/home" exact component={ Layout } />
+          <AuthProvider>
+            <Route path="/" exact component={ Auth } />
+            <Route path="/login" exact component={ Login } />
+          </AuthProvider>
+        </Switch>
       </BrowserRouter>
     </>
   )
