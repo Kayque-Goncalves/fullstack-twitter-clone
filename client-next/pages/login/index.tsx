4204e3407981cc3498ce052dcb9cc244a942d4ad
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-05-04 12:31:24 -0300

    Removes logic test code

--- client-next/pages/login/index.tsx
+++ client-next/pages/login/index.tsx
@@ -5,26 +5,8 @@ import { useSignFlow } from "../../contexts/SignFlowContext"
 
 import styles from './login.module.scss'
 
-import axios from 'axios'
-import { useState } from "react"
-
-export default function Login ({ teste }) {
+export default function Login () {
   const { isModalOpen, toggleModal } = useSignFlow()
-
-  const [userData, setUserData] = useState({
-    name: 'next', loginOption: 'loginNext@test.com', birth: 'October, 31, 2002', month: 'October', day: '31', year: '2002', password: '123', confirmPassword: '123'
-  })
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    try{
-      const response = await axios.post('http://localhost:5000/api/register', userData)
-      console.log(response)
-    } catch (error) {
-      console.log(error)
-    }
-  }
 
   return (
     <div className={ styles.wrapper }>
@@ -42,7 +24,7 @@ export default function Login ({ teste }) {
           <form>
             <input name="loginOption" placeholder="Phone or Email" required />
             <input type="password" name="password" placeholder="Password" required />
-            <button onClick={ handleSubmit }> Log in </button>
+            <button> Log in </button>
           </form>
 
           <span onClick={ toggleModal }> Sign up for Twitter </span>
