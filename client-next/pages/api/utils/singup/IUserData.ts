bd9619efeacc2a94882b874be17ab786fae22453
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-05-04 12:33:50 -0300

    Creates userData interface

---
+++ client-next/pages/api/utils/singup/IUserData.ts
@@ -0,0 +1,10 @@
+export interface IUserData {
+  name: string
+  loginOption?: string 
+  birth?: string 
+  month?: string 
+  day?: string
+  year?: string
+  password?: string
+  confirmPassword?: string
+}
